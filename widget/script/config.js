3c86238a26230c53dcbbfe286acffa62174aefbe
mooonhok <1026413232@qq.com>
2018-06-11 11:34:36 +0800

    修改

--- widget/script/config.js
+++ widget/script/config.js
@@ -1,4 +1,4 @@
 // var url='http://192.168.1.10/cloud_ware-master/';
-//var url='http://api.uminfor.cn/';
+var url='http://api.uminfor.cn/';
 // var url='http://192.168.1.12/cloud_ware-master/';
-var url='http://api.uminfo.cn/';
+//var url='http://api.uminfo.cn/';
